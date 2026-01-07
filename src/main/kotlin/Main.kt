790a1372705d58d12c03527cb23ce63611adc1b9
NiriLorentzen <niri.lorentzen@gmail.com>
2026-01-07 11:19:15 +0100

    utvidet urlBuilder, skrev noe dokumentasjon til funksjonen, la til direction parameter, timeTo og timeFrom.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,11 +8,31 @@ import okhttp3.Callback
 import okhttp3.Call
 import okhttp3.Response
 
-fun urlBuilder(airportCode: String): String {
+fun urlBuilder(airportCode: String, timeFrom: String = "0", timeTo: String = "1", direction: String = ""): String {
+    /*
+     Makes a complete url for the api to use based on the avinor api.
+     Obligatory parameters: airport code, example: OSL
+     non-obligatory parameters:
+        timeFrom, what flights you fetch backwards in time, counts in hours
+        timeTo, what flights you fetch forwards in time, counts in hours
+        direction, shows either arrival flights, departure flights, or both. "A" shows only arrivals, "D" shows only departures, nothing will show both.
+        lastUpdate, shows flightdata only from after a set datetime
+        codeshare, adds possible codeshare information to the flightdata, consists of: codeshareAirlineDesignators, codeshareAirlineNames, codeshareFlightNumbers and codeshareOperationalSuffixs.
+        serviceType, option to differentiate based on flight type, such as helicopter(E), regular flights(J), charter flights(C)
+    */
     val baseurl = "https://asrv.avinor.no/XmlFeed/v1.0"
     val airport = "?airport=" + airportCode
+    val time = "&TimeFrom=" + timeFrom + "&TimeTo=" + timeTo
 
-    val url = baseurl + airport
+    //formats direction-information if a valid direction is specified, else sets it to be nothing
+    val directionFinal = if (direction != "" && (direction == "D" || direction == "A")) {
+        "&Direction=" + direction
+    } else {
+        ""
+    }
+
+
+    val url = baseurl + airport + time + directionFinal
     return url;
 }
 
@@ -21,7 +41,7 @@ fun main() {
     val client = OkHttpClient()
 
     val request = Request.Builder()
-        .url(urlBuilder("OSL"))
+        .url(urlBuilder("OSL", direction="A"))
         .build()
 
     client.newCall(request).enqueue(object : Callback {
