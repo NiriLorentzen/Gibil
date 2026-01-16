d1000683ab0571765c0882b8a77a43889bed5cda
Eivind-HB <eivind.h.breivik@gmail.com>
2026-01-16 10:11:52 +0100

    Added a shared jaxbContext object

---
+++ src/main/kotlin/JaxbContext.kt
@@ -0,0 +1,37 @@
+package org.example
+
+import jakarta.xml.bind.JAXBContext
+import jakarta.xml.bind.Marshaller
+import jakarta.xml.bind.Unmarshaller
+import model.avinorApi.Airport
+import uk.org.siri.siri21.Siri
+
+/**
+ * A shared JAXB context object for the project.
+ * If new classes are needed they can be added to the context variable
+ */
+object JaxbContext {
+
+   val context: JAXBContext = JAXBContext.newInstance(
+       Airport::class.java,
+       Siri::class.java
+   )
+
+    /**
+     * Always encoded to UTF-8
+     * @param formattedOutput boolean. Baseline false, returns compressed string. If set to true returns formatted XML
+     * @return Marshaller
+     */
+    fun createMarshaller(formattedOutput: Boolean = false): Marshaller {
+        return  context.createMarshaller().apply {
+            setProperty(Marshaller.JAXB_ENCODING, "UTF-8")
+            if (formattedOutput) {
+                setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true)
+            }
+        }
+    }
+
+    fun createUnmarshaller(): Unmarshaller {
+        return context.createUnmarshaller()
+    }
+}
